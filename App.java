eb1a14debf7d04327440fa9a777fea83e8d4240b
ydev29 <devyuri29@gmail.com>
2026-04-28 19:58:49 -0300

    Add files via upload

---
+++ App.java
@@ -0,0 +1,18 @@
+// Nome: Yuri Gabriel Carajá de Assunção
+// Matrícula: 1261944881
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        imprimirArray();
+
+    }
+
+    public static void imprimirArray() {
+        int[] numeros = { 10, 20, 30, 40, 50 };
+        int i = 0;
+        for (i = 0; i < numeros.length; i++) {
+            System.out.println(numeros[i]);
+        }
+    }
+
+}
